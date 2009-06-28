a776c7adf1bfe5f0e7b120c7a2571d1df780b353
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2009-06-28 08:50:13 +0200

    authors file + extended setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from distutils.core import setup
 
 setup(
     name='pygtkhelpers',
+    version='0.1',
+    author='Ali Afshar',
+    author_mail='aafshar@gmail.com',
     description='Helper library for PyGTK',
     packages=['pygtkhelpers'],
 )
