2130a9a4c385683ea10f5429d81a29d31a527086
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2009-06-29 12:37:53 +0200

    add example data to the addressbook view example

--- examples/addressbook/view.py
+++ examples/addressbook/view.py
@@ -4,6 +4,7 @@ from pygtkhelpers.utils import run_in_window
 
 from listing import PersonList
 from form import PersonForm
+from person import Person
 
 class PersonEditor(SlaveView):
 
@@ -18,10 +19,15 @@ class PersonEditor(SlaveView):
     def on_listing__item_activated(self, listing, item):
         self.detail.model = item
 
-    def on_detail__model_updated(self, detail):
+    def on_detail__model_updated(self, detail, attribute, value):
         self.listing.objects.update(detail.model)
+
+    def append_item(self, item):
+        self.listing.append_item(item)
 
 if __name__ == '__main__':
     editor = PersonEditor()
+    editor.append_item(Person('Ali', 'Afshar', 'aafshar@gmail.com'))
+    editor.append_item(Person('Hilda', 'Afshar', 'hafshar@gmail.com'))
     run_in_window(editor)
 
