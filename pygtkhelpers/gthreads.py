82d76622274945cc2537b71fbd0e72520c038e50
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2009-08-12 19:32:52 +0200

    fix gthreads license, add initialize warning

--- pygtkhelpers/gthreads.py
+++ pygtkhelpers/gthreads.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*- 
 """
     :copyright: 2005-2008 by The PIDA Project
-    :license: GPL 2 or later (see README/COPYING/LICENSE)
+    :license: LGPL 2 or later (see README/COPYING/LICENSE)
+
+    .. warning:
+
+        this stuff need you to initialize gtk threading!!!
 """
 
 
