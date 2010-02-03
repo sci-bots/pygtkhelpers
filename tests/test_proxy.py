c9ca4767e782f8babcbd47e6b3c5ed6995f3a74b
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-02-03 02:29:31 +0100

    make proxy tests data type independ

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -1,5 +1,5 @@
+import gtk
 from pygtkhelpers.proxy import widget_proxies
-
 
 def pytest_generate_tests(metafunc):
     for widget, proxy in widget_proxies.items():
@@ -8,23 +8,35 @@ def pytest_generate_tests(metafunc):
 def pytest_funcarg__widget(request):
     return request.param[0]()
 
+def pytest_funcarg__attr_type(request):
+    widget, proxy = request.param
+    attr = proxy.prop_name
+    return widget
+    
 
 def pytest_funcarg__proxy(request):
     widget = request.getfuncargvalue('widget')
     return request.param[1](widget)
 
-def pytest_funcarg__attr(request):
-    proxy = request.getfuncargvalue('proxy')
-    return proxy.prop_name
+def pytest_funcarg__value(request):
+    return widget_test_values[request.param[0]]
 
-def test_update(proxy):
-    proxy.update('test')
+widget_test_values = {
+    gtk.Entry: 'test',
+    gtk.ToggleButton: True,
+    gtk.CheckButton: True,
+    gtk.CheckMenuItem: True,
+}
 
 
-def test_update_and_read(proxy):
-    proxy.update('test')
+def test_update(proxy, value):
+    proxy.update(value)
+
+
+def test_update_and_read(proxy, value):
+    proxy.update(value)
     data = proxy.read()
-    assert data == 'test'
+    assert data == value
 
 
 
