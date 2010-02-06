95b2d15e31f9d27700cb91136c829bcea621042a
Ali Afshar <aafshar@gmail.com>
2010-02-06 11:47:21 +0000

    A more robust refresh_gui, with better default values

--- pygtkhelpers/utils.py
+++ pygtkhelpers/utils.py
@@ -136,10 +136,11 @@ def gproperty(name, ptype, default=None, nick='', blurb='',
 
     dict[name] = (ptype, nick, blurb) + default + (flags,)
 
-def refresh_gui(delay=0):
+def refresh_gui(delay=0.1, wait=0.01):
     time.sleep(delay)
     while gtk.events_pending():
         gtk.main_iteration_do(block=False)
+        time.sleep(wait)
 
 
 
