3c55d8c207cf6b64ec27b3a86223eceddd091459
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-05-01 18:04:11 +0200

    ensure that the conftest excepthook wont fail

--- conftest.py
+++ conftest.py
@@ -14,10 +14,12 @@ class Hook(object):
 
 def pytest_runtest_call(item, __multicall__):
     sys.excepthook = Hook(sys.excepthook)
-    __multicall__.execute()
-    hook = sys.excepthook
-    sys.excepthook = hook.oldhook
-    if hook.called:
-        tp, val, tb = hook.args
-        print repr(tp), repr(val), repr(tb)
-        raise tp, val, tb
+    try:
+        __multicall__.execute()
+    finally:
+        hook = sys.excepthook
+        sys.excepthook = hook.oldhook
+        if hook.called:
+            tp, val, tb = hook.args
+            print repr(tp), repr(val), repr(tb)
+            raise tp, val, tb
