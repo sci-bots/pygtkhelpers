d970b8c82f460b5ccdbfcc603003dd04e24e107b
Ali Afshar <aafshar@gmail.com>
2010-05-27 12:57:55 +0100

    Renamed plugins to addons
    --HG--
    rename : pygtkhelpers/plugins.py => pygtkhelpers/addons.py
    rename : tests/test_plugins.py => tests/test_addons.py

---
+++ pygtkhelpers/addons.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+"""
+    pygtkhelpers.addons
+    ~~~~~~~~~~~~~~~~~~~~
+
+    Extending GTK WIdgets (or any GObject) without subclassing them.
+
+    :copyright: 2010 by pygtkhelpers Authors
+    :license: LGPL 2 or later (see README/COPYING/LICENSE)
+
+"""
+
+from pygtkhelpers.utils import GObjectUserDataProxy
+
+
+def apply_addons(widget, *addon_types, **named_addon_types):
+    """Apply some addons to a widget.
+
+    :param widget: The widget to apply addons to.
+    :param addon_types: A list of addon types, which will be instantiated
+                         and applied to the widget with the default name of
+                         the addon.
+    :param named_addon_types: A named list of addons, the keywords will be
+                               the name of the addon when loaded and will
+                               override the default addon name. This can
+                               allow loading the same addon multpily for the
+                               same widget under different names.
+
+    Plugins should conform to the GObjectPlugin interface or be a subclass of
+    it. Once loaded, addons will be available as widget.addons.<addon_name>
+    withe standard attribute access.
+    """
+    if not hasattr(widget, 'addons'):
+        widget.addons = GObjectUserDataProxy(widget)
+    for addon_type in addon_types:
+        addon = addon_type(widget)
+        setattr(widget.addons, addon.addon_name, addon)
+    for name, addon_type in named_addon_types.items():
+        addon = addon_type(widget, addon_name=name)
+        setattr(widget.addons, addon.addon_name, addon)
+
+
+class GObjectPlugin(object):
+    """A base class for addons for GObjects
+
+    :param widget: The widget to apply the addon to
+    :param addon_name: Optional name to override the class `addon_name`
+                        attribute to load a addon under a non-default name.
+
+    .. note::
+
+        Either the class's addon_name should be set or addon_name must be
+        passed to the constructor, otherwise `ValueError` will be raised.
+    """
+
+    #: The default addon name for instances of this addon
+    addon_name = None
+
+    def __init__(self, widget, addon_name=None):
+        self.widget = widget
+        self.addon_name = addon_name or self.addon_name
+        if self.addon_name is None:
+            raise ValueError('addon_name must be set.')
+        self.create_ui()
+
+    def create_ui(self):
+        """Create the user interface
+
+        For overriding in implementations.
+        """
+
---
+++ tests/test_addons.py
@@ -0,0 +1,39 @@
+
+import gtk
+
+from pygtkhelpers.addons import GObjectPlugin, apply_addons
+
+
+class OkButtonPlugin(GObjectPlugin):
+    """A addon to make a button into an OK Button
+    """
+    addon_name = 'ok_button'
+
+    def create_ui(self):
+        self.widget.set_use_stock(True)
+        self.widget.set_label(gtk.STOCK_OK)
+
+
+def test_addon_apply():
+    w = gtk.Button()
+    apply_addons(w, OkButtonPlugin)
+    assert w.addons.ok_button
+    assert w.get_label() == gtk.STOCK_OK
+    assert w.get_use_stock()
+
+def test_named_addon_apply():
+    w = gtk.Button()
+    apply_addons(w, dummy=OkButtonPlugin)
+    assert w.addons.dummy
+    assert w.get_label() == gtk.STOCK_OK
+    assert w.get_use_stock()
+
+def test_named_multiple_addons_apply():
+    w = gtk.Button()
+    apply_addons(w, OkButtonPlugin, dummy=OkButtonPlugin)
+    assert w.addons.dummy
+    assert w.addons.ok_button
+    assert w.get_label() == gtk.STOCK_OK
+    assert w.get_use_stock()
+
+
