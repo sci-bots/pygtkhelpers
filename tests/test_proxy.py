d1d823dabe2cf1dc222eff33226b50e238c2fa8f
Ali Afshar <aafshar@gmail.com>
2010-05-29 16:21:12 +0100

    restore the skipped gtk.FileChooserButton proxy test

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -83,7 +83,7 @@ widget_test_values = {
     StringList: ['hans', 'peter'],
     gtk.ComboBox: 'test',
     SimpleComboBox: 'test',
-    #gtk.FileChooserButton: __file__,
+    gtk.FileChooserButton: __file__,
     gtk.FileChooserWidget: __file__,
     gtk.FontButton: 'Monospace 10',
     gtk.Label: 'Hello',
@@ -99,7 +99,10 @@ def test_update(proxy, value):
 
 def test_update_and_read(proxy, value):
     proxy.update(value)
-    refresh_gui()
+    if isinstance(proxy.widget, gtk.FileChooserButton):
+        refresh_gui(0.1, 0.1)
+    else:
+        refresh_gui()
     data = proxy.read()
     assert data == value
 
