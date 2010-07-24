0d8264a95f7a70fec46d48f98f6d120b4c5626f7
DasIch <dasdasich@gmail.com>
2010-07-25 00:40:59 +0200

    Fixed a typo

--- examples/manual/d1.py
+++ examples/manual/d1.py
@@ -5,7 +5,7 @@ import gtk
 from pygtkhelpers.delegates import WindowView
 
 class UserView(WindowView):
-    """The user interface for my user management ogram"""
+    """The user interface for my user management program"""
 
 if __name__ == '__main__':
     UserView().show_and_run()
