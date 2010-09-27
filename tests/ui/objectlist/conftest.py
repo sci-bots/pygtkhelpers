1cd1ded22a7aaebdfa903ad295ecdb488fa33df4
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-09-27 16:44:21 +0200

    move the searchcheck funcarg to the search tests

--- tests/ui/objectlist/conftest.py
+++ tests/ui/objectlist/conftest.py
@@ -37,28 +37,3 @@ def pytest_funcarg__user2(request):
 def pytest_funcarg__user3(request):
     return User(name='Witch', age=409)
 
-def pytest_funcarg__searchcheck(request):
-    items = request.getfuncargvalue('items')
-    return SearchChecker(items)
-
-class SearchChecker(object):
-    def __init__(self, ol):
-        self.ol = ol
-        self.entry = gtk.Entry()
-        self.ol.set_search_entry(self.entry)
-        self.w = gtk.Window()
-        vb = gtk.VBox()
-        self.w.add(vb)
-        vb.pack_start(self.ol)
-        vb.pack_start(self.entry)
-        self.ol.extend([
-            User(name='a', age=1),
-            User(name='B', age=2),
-            User(name='c', age=3),
-        ])
-        self.u1, self.u2, self.u3 = tuple(ol)
-
-    def assert_selects(self, key, item):
-        self.entry.set_text(key)
-        refresh_gui()
-        assert self.ol.selected_item is item
--- tests/ui/objectlist/test_search.py
+++ tests/ui/objectlist/test_search.py
@@ -1,3 +1,32 @@
+import gtk
+from pygtkhelpers.utils import refresh_gui
+from .conftest import User
+
+def pytest_funcarg__searchcheck(request):
+    items = request.getfuncargvalue('items')
+    return SearchChecker(items)
+
+class SearchChecker(object):
+    def __init__(self, ol):
+        self.ol = ol
+        self.entry = gtk.Entry()
+        self.ol.set_search_entry(self.entry)
+        self.w = gtk.Window()
+        vb = gtk.VBox()
+        self.w.add(vb)
+        vb.pack_start(self.ol)
+        vb.pack_start(self.entry)
+        self.ol.extend([
+            User(name='a', age=1),
+            User(name='B', age=2),
+            User(name='c', age=3),
+        ])
+        self.u1, self.u2, self.u3 = tuple(ol)
+
+    def assert_selects(self, key, item):
+        self.entry.set_text(key)
+        refresh_gui()
+        assert self.ol.selected_item is item
 
 def test_search_col(searchcheck):
     searchcheck.assert_selects('a', searchcheck.u1)
