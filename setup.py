c68c5d03d170adcb8169f9e1fa64929c4b9e97b5
Christian Fobel <christian@fobel.net>
2014-03-19 09:37:59 -0400

    Add auto git-describe version for Python packag...
    Add automatic Python package versioning using `git-describe` through the
    `version.py` module from [here][1].
    
    A Python package source distribution can be built using the following
    command:
    
        python setup.py sdist
    
    __NB__ The version strings generated by `version.getVersion()` conform
    to [PEP386][2].
    
    [1]: https://gist.github.com/mina86/8782771
    [2]: http://www.python.org/dev/peps/pep-0386

--- setup.py
+++ setup.py
@@ -3,11 +3,21 @@ try:
 except ImportError:
     from distutils.core import setup
 
+import version
+
+
 short_description = """
 PyGTKHelpers is a library to assist the building of PyGTK applications.
 """.strip()
 
 long_description = """
+# Note #
+
+This project is a fork of [this project][1], written by Ali Afshar
+<aafshar@gmail.com>.
+
+# Description #
+
 PyGTKHelpers is a library to assist the building of PyGTK applications. It is
 intentionally designed to be *non-frameworky*, and blend well with your
 particular style of PyGTK development.
@@ -17,14 +27,17 @@ delegation, MVC, mixed GtkBuilder/Python views, widget proxying, signal
 auto-connection, object-base lists and trees, a number of helper widgets,
 utility functions for assisting creating new GObject types, unit testing
 helpers and utilities to help debug PyGTK applications.
+
+
+[1]: https://pythonhosted.org/pygtkhelpers/
 """.strip()
 
 setup(
-    name='pygtkhelpers',
-    version='0.4.3',
-    author='Ali Afshar',
-    author_email='aafshar@gmail.com',
-    url='http://packages.python.org/pygtkhelpers',
+    name='pygtk_helpers',
+    version=version.getVersion(),
+    author='Christian Fobel',
+    author_email='christian@fobel.net',
+    url='https://github.com/wheeler-microfluidics/pygtk_helpers',
     description=short_description,
     long_description=long_description,
     license='LGPL-3.0',
