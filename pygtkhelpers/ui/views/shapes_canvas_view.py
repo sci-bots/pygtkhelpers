7e29362636b706bd93bc0fbc64446adbfb519fca
Christian Fobel <christian@fobel.net>
2016-01-04 14:55:02 -0500

    Add render method to GtkShapesCanvasView

--- pygtkhelpers/ui/views/shapes_canvas_view.py
+++ pygtkhelpers/ui/views/shapes_canvas_view.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import cairo
 import gtk
 from svg_model import svg_polygons_to_df
 import pandas as pd
@@ -14,6 +15,7 @@ class GtkShapesCanvasView(GtkCairoView):
         self.shape_i_columns = shape_i_columns
         self.padding_fraction = padding_fraction
         self._canvas_reset_request = False
+        self.cairo_surface = None
         super(GtkShapesCanvasView, self).__init__(**kwargs)
 
     @classmethod
@@ -30,10 +32,14 @@ class GtkShapesCanvasView(GtkCairoView):
                                gtk.gdk.BUTTON_RELEASE_MASK |
                                gtk.gdk.POINTER_MOTION_MASK)
 
-    def draw_shapes(self):
-        cairo_context = self.widget.window.cairo_create()
+    def render_shapes(self, df_shapes=None, cairo_context=None, clip=False):
+        if cairo_context is None:
+            cairo_context = self.widget.window.cairo_create()
 
-        for path_id, df_path_i in (self.canvas.df_canvas_shapes
+        if df_shapes is None:
+            df_shapes = self.canvas.df_canvas_shapes
+
+        for path_id, df_path_i in (df_shapes
                                    .groupby(self.canvas
                                             .shape_i_columns)[['x', 'y']]):
             cairo_context.move_to(*df_path_i.iloc[0][['x', 'y']])
@@ -43,14 +49,30 @@ class GtkShapesCanvasView(GtkCairoView):
             cairo_context.set_source_rgb(0, 0, 1)
             cairo_context.fill()
 
-    def on_canvas_reset_tick(self, width, height):
+    def reset_canvas(self, width, height):
         canvas_shape = pd.Series([width, height], index=['width', 'height'])
         self.canvas = ShapesCanvas(self.df_shapes, self.shape_i_columns,
                                    canvas_shape=canvas_shape,
                                    padding_fraction=self.padding_fraction)
+
+    def on_canvas_reset_tick(self, width, height):
+        self.reset_canvas(width, height)
+        self.render()
         self._canvas_reset_request = False
         gtk.idle_add(self.widget.queue_draw)
         return False
+
+    def reset_cairo_surface(self):
+        # Render shapes to off-screen Cairo surface.
+        self.cairo_surface = cairo.ImageSurface(cairo.FORMAT_ARGB32,
+                                                self.canvas.canvas_shape.width,
+                                                self.canvas.canvas_shape
+                                                .height)
+
+    def render(self):
+        self.reset_cairo_surface()
+        cairo_context = cairo.Context(self.cairo_surface)
+        self.render_shapes(cairo_context=cairo_context)
 
     def on_widget__configure_event(self, widget, event):
         '''
@@ -59,7 +81,8 @@ class GtkShapesCanvasView(GtkCairoView):
         if event.x < 0 and event.y < 0:
             # Widget has not been allocated a size yet, so do nothing.
             return
-        # Use `self._canvas_reset_request` latch to throttle configure event handling.
+        # Use `self._canvas_reset_request` latch to throttle configure event
+        # handling.
         # This makes the UI more responsive when resizing the drawing area, for
         # example, by dragging the window border.
         if not self._canvas_reset_request:
@@ -73,10 +96,17 @@ class GtkShapesCanvasView(GtkCairoView):
         Called when drawing area is first displayed and, for example, when part
         of drawing area is uncovered after being covered up by another window.
         '''
-        if self.canvas is None:
-            return
-        # Draw shapes from SVG.
-        self.draw_shapes()
+        # Paint pre-rendered off-screen Cairo surface to drawing area widget.
+        self.draw()
+
+    def draw(self):
+        print '[draw]'
+        if self.cairo_surface is not None:
+            cairo_context = self.widget.window.cairo_create()
+            cairo_context.set_source_surface(self.cairo_surface, 0, 0)
+            cairo_context.paint()
+        else:
+            print '    No cairo surface'
 
 
 def parse_args(args=None):
