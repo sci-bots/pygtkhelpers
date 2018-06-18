be19b44baf4060fd2d69f64d526c8b95cdc10183
Christian Fobel <christian@fobel.net>
2018-06-18 11:17:06 -0400

    fix(pandas): do not use `pandas.Series.argmin`
    Prior to this commit, the following deprecation warning occurs when using
    `pandas>=0.22.0`:
    
    ```
    FutureWarning: 'argmin' is deprecated. Use 'idxmin' instead. The behavior of 'argmin' will be corrected to return the positional minimum in the future. Use 'series.values.argmin' to get the position of the minimum now.
    ```
    
    In this commit, discontinue use of deprecated `pandas.Series.argmin` in
    `ui.views.find_closest`.

--- pygtkhelpers/ui/views/__init__.py
+++ pygtkhelpers/ui/views/__init__.py
@@ -1,2 +1,21 @@
 def find_closest(df_points, point):
-    return df_points.iloc[((df_points - point) ** 2).sum(axis=1).argmin()]
+    '''
+    Parameters
+    ----------
+    df_points : pandas.DataFrame
+        Table with at least ``x`` and ``y`` columns.
+    point : pandas.Series
+        Series with at least ``x`` and ``y`` keys.
+
+    Returns
+    -------
+    pandas.Series
+        Row of :data:`df_points` table with point closest to specified
+        :data:`point`.
+
+
+    .. versionchanged:: X.X.X
+        Discontinue use of deprecated ``pandas.Series.argmin``.
+    '''
+    return df_points.iloc[((df_points - point) ** 2).sum(axis=1).values
+                          .argmin()]
